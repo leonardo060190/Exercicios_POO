9b227bae81d681af388e302b257fe0b24931b020
Leonardo Domingos Patricio <leonardo_patricio@estudante.sesisenai.org.br>
2024-03-21 21:49:20 -0300

    exercicios de classe

--- src/Temperatura/Temperatura.java
+++ src/Temperatura/Temperatura.java
@@ -5,9 +5,11 @@ public class Temperatura {
     double celsius;
 
     public void celsiusParaFahrenheit(double celsius){
-        double result1 = (this.celsius * 9/5) + 32;
-        System.out.println("A temperatura " + this.celsius + " em celsius equivale a " + result1 + " em fahrenheit");
+        double result1 = (celsius * 9/5) + 32;
+        System.out.println("A temperatura " + celsius + " em celsius equivale a " + result1 + " em fahrenheit");
 
     }
+
+
 
 }
