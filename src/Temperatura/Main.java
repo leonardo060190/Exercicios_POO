3d01981c7f9974abcf82f594f23de065465b55d0
Leonardo Domingos Patricio <leonardo_patricio@estudante.sesisenai.org.br>
2024-03-22 19:11:31 -0300

    feito a conversão de temperatura do exercicios de classe

--- src/Temperatura/Main.java
+++ src/Temperatura/Main.java
@@ -9,10 +9,15 @@ public class Main {
 
         Scanner leia = new Scanner(System.in);
 
-        System.out.println("Informe a temperatura: ");
+        System.out.println("Informe a temperatura em celsius: ");
         double celsius = leia.nextDouble();
 
         temperatura.celsiusParaFahrenheit(celsius);
+
+        System.out.println("Informe a temperatura em fahrenheit: ");
+        double fahrenheit = leia.nextDouble();
+
+        temperatura.fahrenheitParaCelsius(fahrenheit);
 
     }
 }
--- src/Temperatura/Temperatura.java
+++ src/Temperatura/Temperatura.java
@@ -4,10 +4,17 @@ public class Temperatura {
 
     double celsius;
 
+    double fahrenheit;
+
     public void celsiusParaFahrenheit(double celsius){
         double result1 = (celsius * 9/5) + 32;
         System.out.println("A temperatura " + celsius + " em celsius equivale a " + result1 + " em fahrenheit");
 
+    }
+
+    public void fahrenheitParaCelsius(double fahrenheit){
+        double result2 = (fahrenheit - 32) *5/9;
+        System.out.println("A temperatura " + fahrenheit + " em fahrenheit equivale a " + result2 + " em celsius");
     }
 
 
